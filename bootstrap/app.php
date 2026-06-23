a7d8138fb4859647b9cbb2ae8f01183e53351f4b
luisangelespinozamauro <luis.espinoza@ldrsolutions.com.mx>
2026-06-23 16:45:44 -0600

    cambios

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,19 +3,36 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        Authenticate::redirectUsing(function () {
+            return null;
+        });
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
-    })->create();
+
+        $exceptions->render(function (
+            AuthenticationException $e,
+            Request $request
+        ) {
+
+            return response()->json([
+                'message' => 'No autorizado'
+            ], 401);
+
+        });
+
+    })
+    ->create();
